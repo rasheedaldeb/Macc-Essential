ec331b050ad9ec4d7a7f1de709b0cfca08407989
rasheedaldeb <rasheedaldeb@gmail.com>
2024-10-21 14:49:38 +0300

    first commit

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,7 @@ import "./index.css";
 import { BrowserRouter } from "react-router-dom";
 
 createRoot(document.getElementById("root")).render(
-  <BrowserRouter basename="/macc-essential">
+  <BrowserRouter>
     <StrictMode>
       <App />
     </StrictMode>
